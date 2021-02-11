898a042f70179f087ada18cebcd5169af8366d76
mrpojam <jafari.pouya79@outlook.com>
2021-02-11 11:21:55 +0330

    Added signals and extended user profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(null = True, blank = True)
+    bio = models.TextField(max_length=500, null = True, blank = True)
+    thumbnail = models.ImageField(upload_to = 'media', null = True, blank = True)
+
+    def __str__(self):
+        return self.user.username
+def save_profile(sender, **kwargs):
+    if kwargs['created']:
+        p1 = Profile(user=kwargs['instance'])
+        p1.save()
+post_save.connect(save_profile, sender = User)

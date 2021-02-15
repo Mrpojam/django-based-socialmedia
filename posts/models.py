9acf963979ac46fbd04290fff671bdf2b1a5b0c3
mrpojam <jafari.pouya79@outlook.com>
2021-02-16 00:02:13 +0330

    created comments model

--- posts/models.py
+++ posts/models.py
@@ -16,3 +16,14 @@ class Post (models.Model):
          self.created.month,
           self.created.day,
            self.slug])
+
+class Comment (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomment')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body}'
